6a6e700e331b09555610204c71908b6dba4190ef
bvpav <bvpav@tutanota.com>
2022-03-11 14:33:28 +0200

    Added props to UserAvatar

--- web/components/UserAvatar.jsx
+++ web/components/UserAvatar.jsx
@@ -1,8 +1,8 @@
 import { Avatar } from '@mantine/core';
 
-const UserAvatar = ({ username }) => {
+const UserAvatar = ({ username, ...props }) => {
   return (
-    <Avatar radius="xl" color="grape">
+    <Avatar radius="xl" color="grape" {...props}>
       {username.charAt(0).toUpperCase()}
     </Avatar>
   );
