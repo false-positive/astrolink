28873c663e1860926b8bc66f3b2b4a73d0b52e93
Kerzoken <kerzo.gm@gmail.com>
2022-03-11 17:35:23 +0200

    Added All Files Page

--- web/components/FileList.jsx
+++ web/components/FileList.jsx
@@ -1,6 +1,5 @@
 import { Box, createStyles, Group } from '@mantine/core';
 import { AiFillFileText } from 'react-icons/ai';
-import Sidebar from './Sidebar';
 
 const useStyles = createStyles((theme, _params, getRef) => {
   const fileboxColor =
@@ -9,9 +8,9 @@ const useStyles = createStyles((theme, _params, getRef) => {
     filebox: {
       cursor: 'pointer',
       background: fileboxColor,
-      width: '25rem',
+      width: '100%',
       padding: '0.5rem 0.75rem 0.5rem 0.75rem',
-      fontSize: '1.1rem',
+      fontSize: '1.3rem',
       borderRadius: '0.5rem',
       display: 'flex',
       flexDirection: 'row',
@@ -22,7 +21,8 @@ const useStyles = createStyles((theme, _params, getRef) => {
     },
   };
 });
-const FileSidebar = ({ files }) => {
+
+const FileList = ({ files }) => {
   const getDateWords = (date) => {
     const month = date.toLocaleString('default', { month: 'long' });
     const day = date.getDate();
@@ -32,29 +32,22 @@ const FileSidebar = ({ files }) => {
 
   const { classes } = useStyles();
   return (
-    <Sidebar side="right">
-      <Group direction="column">
-        {files.map((file) => (
-          <Box className={classes.filebox} key={file.id}>
-            <Group
-              direction="column"
-              position="apart"
-              spacing="xs"
-              style={{ width: '100%' }}
-            >
-              <div>
-                <Box as="span" sx={{ display: 'inline' }} px={2}>
-                  <AiFillFileText className={classes.fileicon} />
-                </Box>
-                {file.name}
-              </div>
-              Last Modified {getDateWords(new Date(file.lastModified))}
-            </Group>
-          </Box>
-        ))}
-      </Group>
-    </Sidebar>
+    <Group>
+      {files.map((file) => (
+        <Box className={classes.filebox} key={file.id}>
+          <Group position="apart" spacing="xs" style={{ width: '100%' }}>
+            <div>
+              <Box as="span" sx={{ display: 'inline' }} px={2}>
+                <AiFillFileText className={classes.fileicon} />
+              </Box>
+              {file.name}
+            </div>
+            Last Modified {getDateWords(new Date(file.lastModified))}
+          </Group>
+        </Box>
+      ))}
+    </Group>
   );
 };
 
-export default FileSidebar;
+export default FileList;
