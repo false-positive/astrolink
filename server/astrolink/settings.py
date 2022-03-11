74318ec6c8efac1162cd911a4fe692227158f5cd
bvpav <bvpav@tutanota.com>
2022-03-11 11:20:38 +0200

    Setup django project

--- server/astrolink/settings.py
+++ server/astrolink/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Contribs
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+    'rest_framework',
+
+    # In-house
+    'api',
 ]
 
 MIDDLEWARE = [
