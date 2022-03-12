cb4585452d0b9146cd9b85b455ab9c3d798996a7
CodenameSource <kaloyanmiladinov@gmail.com>
2022-03-13 00:53:29 +0200

    Replaced file's project serializer field from RelatedString to PrimaryKey

--- server/files/serializers.py
+++ server/files/serializers.py
@@ -8,14 +8,14 @@ class RevSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rev
-        fields = ['name', 'parent', 'file', 'mimetype', 'revision', 'date_added', 'date_changed']
+        fields = ['name', 'parent', 'file', 'mimetype', 'revision', 'created_at', 'last_modified']
 
 
 class FileSerializer(serializers.ModelSerializer):
-    project = serializers.StringRelatedField(read_only=True)
+    project = serializers.PrimaryKeyRelatedField(read_only=True)
     revisions = RevSerializer(source='rev_set', many=True, read_only=True)
 
     class Meta:
         model = File
         fields = ['project', 'name', 'mimetype',
-                  'file', 'revisions', 'query_id', 'date_added', 'date_changed']
+                  'file', 'revisions', 'query_id', 'created_at', 'last_modified']
