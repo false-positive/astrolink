1b2a0dc0194c4e2c664507bb3ddc56b7aec35df4
bvpav <bvpav@tutanota.com>
2022-03-12 10:37:44 +0200

    Added API teams functions
    Co-authored-by: Kerzoken <Kerzoken@users.noreply.github.com>

---
+++ web/api/team.js
@@ -0,0 +1,25 @@
+import makeRequest from './request';
+
+const fromApiTeam = ({ uuid, ...rest }) => ({ ...rest, id: uuid });
+const toApiTeam = ({ id, ...rest }) => ({ ...rest, uuid: id });
+
+export const getTeam = async (uuid) => {
+  const response = await makeRequest(`/teams/${uuid}`);
+  const apiTeam = await response.json();
+  return fromApiTeam(apiTeam);
+};
+
+export const setTeam = async (team) => {
+  let apiTeam = toApiTeam(team);
+  const { uuid } = apiTeam;
+  const response = await makeRequest(`/teams/${uuid}`, {
+    method: 'PATCH',
+    body: JSON.stringify(apiTeam),
+  });
+  if (response.ok) {
+    apiTeam = await response.json();
+    return fromApiTeam(team);
+  }
+  const errors = await response.json();
+  throw errors;
+};
