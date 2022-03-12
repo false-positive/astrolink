6eabc8309a7eb72b4dd4c0809db433a4bc1688fd
CodenameSource <kaloyanmiladinov@gmail.com>
2022-03-12 14:11:11 +0200

    Added revision serializer

--- server/files/serializers.py
+++ server/files/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from files.models import File
-
+from files.models import File, Rev
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ['name', 'project', 'file']
+
+
+class RevSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rev
+        fields = ['name', 'parent', 'file']
+
+
+
+
