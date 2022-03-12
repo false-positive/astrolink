7ccce0bef74313fc72bc9e19452091890c006acc
bvpav <bvpav@tutanota.com>
2022-03-12 14:01:48 +0200

    Added FileMenu in most places where there's a file

--- web/components/FileList.jsx
+++ web/components/FileList.jsx
@@ -1,7 +1,8 @@
 import { Box, createStyles, Group } from '@mantine/core';
 import { AiFillFileText } from 'react-icons/ai';
+import FileMenu from './FileMenu';
 
-const useStyles = createStyles((theme, _params, getRef) => {
+const useStyles = createStyles((theme, _params, _getRef) => {
   const fileboxColor =
     theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[1];
   return {
@@ -44,6 +45,7 @@ const FileList = ({ files, lastModified = true }) => {
             </div>
             {lastModified &&
               `Last Modified ${getDateWords(new Date(file.lastModified))}`}
+            <FileMenu file={file} />
           </Group>
         </Box>
       ))}
