a0241f983fd7dceb5fe3b22334080e5ad3a03d4f
Kerzoken <kerzo.gm@gmail.com>
2022-03-13 01:36:36 +0200

    Added button and links in file sidebar in Project Detail Page

--- web/components/FileSidebar.jsx
+++ web/components/FileSidebar.jsx
@@ -1,4 +1,6 @@
-import { Box, createStyles, Group } from '@mantine/core';
+import { Box, Button, Center, createStyles, Group } from '@mantine/core';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { AiFillFileText } from 'react-icons/ai';
 import Sidebar from './Sidebar';
 
@@ -31,26 +33,44 @@ const FileSidebar = ({ files }) => {
   };
 
   const { classes } = useStyles();
+  const router = useRouter();
+
   return (
     <Sidebar side="right">
       <Group direction="column">
+        <Center sx={{ width: '100%' }}>
+          <Link href={`${router.asPath}/files`} passHref>
+            <Button size="md" as="a" radius="md" variant="default" color="dark">
+              View All Files
+            </Button>
+          </Link>
+        </Center>
+
         {files.map((file) => (
-          <Box className={classes.filebox} key={file.id}>
-            <Group
-              direction="column"
-              position="apart"
-              spacing="xs"
-              style={{ width: '100%' }}
-            >
-              <div>
-                <Box as="span" sx={{ display: 'inline' }} px={2}>
-                  <AiFillFileText className={classes.fileicon} />
-                </Box>
-                {file.name}
-              </div>
-              Last Modified {getDateWords(new Date(file.lastModified))}
-            </Group>
-          </Box>
+          <Link
+            href={`${router.asPath}/files/${file.id}`}
+            passHref
+            key={file.id}
+          >
+            <Box className={classes.filebox}>
+              <Group
+                direction="column"
+                position="apart"
+                spacing="xs"
+                style={{ width: '100%' }}
+              >
+                <div>
+                  <Box as="span" sx={{ display: 'inline' }} px={2}>
+                    <AiFillFileText className={classes.fileicon} />
+                  </Box>
+                  {file.name}
+                </div>
+                <div>
+                  Last Modified {getDateWords(new Date(file.lastModified))}
+                </div>
+              </Group>
+            </Box>
+          </Link>
         ))}
       </Group>
     </Sidebar>
